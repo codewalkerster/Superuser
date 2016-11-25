7871085fdfed920011c8b10b0b300b005fa615fc
codewalker <codewalker@hardkernel.com>
2016-11-25 13:58:35 +0900

    ODROID-C2: Fix build error on 6.0.1
    Change-Id: If51c666413adf237c7c3dca52ec3572b5baa626f

--- Superuser/Android.mk
+++ Superuser/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src) $(call all-java-files-under,../../Widgets/Widgets/src)
+LOCAL_JAVA_LIBRARIES += org.apache.http.legacy
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --extra-packages com.koushikdutta.widgets -S $(LOCAL_PATH)/../../Widgets/Widgets/res --auto-add-overlay --rename-manifest-package $(SUPERUSER_PACKAGE)
